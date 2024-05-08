d565fa16621a600f7c0b47db961d3dd44b5b4818
Stephen Boynton <boyntonsc@gmail.com>
2024-05-08 17:13:43 -0400

    feat: working go (#2)

--- rp-go/main.go
+++ rp-go/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
- func main() {
-    fmt.Printf("Starting web app at now 8080 90\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!\n")
+}
+
+func headers(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	fmt.Printf("Starting web app at now 8000, you know, the PORT\n")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	err := http.ListenAndServe(":8000", nil)
+
+	if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
