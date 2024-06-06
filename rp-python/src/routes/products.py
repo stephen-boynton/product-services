41e51512c1226f02a0101b1f1da323339f794b32
Stephen Boynton <boyntonsc@gmail.com>
2024-06-06 07:58:48 -0400

    feat: get products

--- rp-python/src/routes/products.py
+++ rp-python/src/routes/products.py
@@ -1,7 +1,17 @@
 from flask import Blueprint
+import json
+import sys
+import os
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the JSON file in the sibling directory
+json_file_path = os.path.join(current_dir, '..', 'data', 'products.json')
+with open(json_file_path, 'r') as json_file:
+    products_data = json.load(json_file)
 
 products = Blueprint('products', __name__)
 
 @products.route('/products', methods=['GET'])
 def products_get():
-    return "Hello from products 1"
+    return products_data
